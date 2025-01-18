e3112c9995848c340a733f21efbf7fc6633201ce
justaproger <93209186+justaproger@users.noreply.github.com>
2025-01-19 01:21:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from urllib.parse import unquote
 app = Flask(__name__)
 CORS(app)  # Включаем CORS для всего приложения
 
+
 # Загружаем вопросы из JSON-файла
 def load_questions():
     json_file_path = 'questions.json'
@@ -16,24 +17,27 @@ def load_questions():
         questions = json.load(file)
     return questions
 
+
 questions = load_questions()
+
 
 @app.route('/<question>', methods=['GET'])
 def find_answer(question):
     try:
         question_text = unquote(question)  # Декодируем URL-encoded строку
-        
+
         if not question_text:
             return jsonify({"error": "Question is required"}), 400
-        
+
         match = next((q for q in questions if q['question'] == question_text), None)
-        
+
         if match:
             return jsonify({"answer": match['correct_answer']}), 200
         else:
             return jsonify({"error": "Question not found"}), 404
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
