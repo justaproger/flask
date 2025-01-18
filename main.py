a0f03ec33c073aa3dade944b4993458362c0ae7c
justaproger <93209186+justaproger@users.noreply.github.com>
2025-01-19 00:16:18 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+import json
 import os
+from urllib.parse import unquote
 
 app = Flask(__name__)
+CORS(app)  # Включаем CORS для всего приложения
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+# Загружаем вопросы из JSON-файла
+def load_questions():
+    json_file_path = 'questions.json'
+    if not os.path.exists(json_file_path):
+        return []
+    with open(json_file_path, 'r', encoding='utf-8') as file:
+        questions = json.load(file)
+    return questions
+
+
+questions = load_questions()
+
+
+@app.route('/<question>', methods=['GET'])
+def find_answer(question):
+    try:
+        question_text = unquote(question)  # Декодируем URL-encoded строку
+
+        if not question_text:
+            return jsonify({"error": "Question is required"}), 400
+
+        match = next((q for q in questions if q['question'] == question_text), None)
+
+        if match:
+            return jsonify({"answer": match['correct_answer']}), 200
+        else:
+            return jsonify({"error": "Question not found"}), 404
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, threaded=True)
